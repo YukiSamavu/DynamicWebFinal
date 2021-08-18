5d82b1c466b5a4aaffde24b504207ec0dd0bac28
YukiSamavu <61423439+YukiSamavu@users.noreply.github.com>
2021-08-18 08:39:20 -0600

    Added things

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const routes = require('./routes/routes');
 
 const app = express();
@@ -10,12 +11,14 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
 let urlendcodedParser  = express.urlencoded({
     extended: false
 });
 
 app.get('/', routes.index);
+app.get('/home', routes.home);
 app.get('/create', routes.create);
 app.post('/create', urlendcodedParser, routes.createAccount);
 app.get('/edit', routes.edit)
